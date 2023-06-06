e77c0f33c6a3b6581a09ec2d595a117d9fecd323
sayandip10 <sayandip.ghosh@astcorporation.com>
2023-06-06 22:13:20 +0530

    second commit

---
+++ src/pages/Register.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Add from "../img/addAvatar.png";
+export const Register = () => {
+  return (
+    <div  className='formContainer'>
+        <div className='formWrapper'>
+            <span className='logo'>Rapid Chat</span>
+            <span className='title'>Register</span>
+            <form action="">
+                <input type="text" placeholder='display name'/>
+                <input type="email" placeholder='email'/>
+                <input type="password" placeholder='password'/>
+                <input style={{display:"none"}} type="file" id='file'/>
+                <label htmlFor="file">
+                    <img src={Add} alt="" />
+                    <span>Add an avatar</span>
+                </label>
+                <button>Sign Up</button>
+            </form>
+            <p>You do have an acoount? Login</p>
+        </div>
+    </div>
+  )
+}
